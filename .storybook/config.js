73099ca5a05fb5b4631d1d5073606f09aecf4913
fagner-daitan <faraujo@daitan.com>
2019-03-28 14:48:20 -0300

    refact: Import style globaly on storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,5 @@
 import { configure } from '@storybook/vue';
+import '../src/styles/main.scss';
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /\.stories\.js$/);
--- stories/components/main-menu/main-menu.stories.js
+++ stories/components/main-menu/main-menu.stories.js
@@ -1,7 +1,5 @@
 import { storiesOf } from "@storybook/vue";
-import { linkTo } from "@storybook/addon-links";
 import MainMenu from '../../../src/components/main-menu/main-menu';
-import '../../../src/styles/main.scss';
 
 storiesOf("MainMenu", module)
   .add("default", () => ({
