bd9661e7ae3128b3368d450109cd75883e781839
iFgR <ifgr.fagner@gmail.com>
2019-03-29 00:17:34 -0300

    feat: Add plans price

--- src/components/plans-price/plans-price.vue
+++ src/components/plans-price/plans-price.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="plan-price">
+  <section class="plans-price">
     <h2 class="title">Tecnologia</h2>
     <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, quis labore! Maxime voluptate aut impedit nisi delectus unde, ipsum doloribus odit aliquam consequatur sapiente harum id soluta quis amet eligendi.</p>
   </section>
@@ -7,6 +7,6 @@
 
 <script>
 export default {
-  name: 'PlanPrice'
+  name: 'PlansPrice'
 }
 </script>
