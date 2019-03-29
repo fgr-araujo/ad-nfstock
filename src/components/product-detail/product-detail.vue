bbaa3394721c9b15d049f837da013d2808d36fe5
iFgR <ifgr.fagner@gmail.com>
2019-03-29 02:16:14 -0300

    feat: improve product detail

--- src/components/product-detail/product-detail.vue
+++ src/components/product-detail/product-detail.vue
@@ -1,18 +1,17 @@
 <template>
   <section class="product-detail">
     <div class="product">
-      <h2 class="title">Tecnologia</h2>
+      <h2 class="title"><i class="icon fas fa-microchip"></i>Tecnologia</h2>
       <p class="description">Chega de ligar várias vezes para seus clientes cobrando as notas eletrônicas. O NF-Stock Alterdata faz a importação automática de todas as notas recebidas por seus clientes direto do site da Receita Federal, utilizando o certificado digital.</p>
     </div>
 
     <div class="product">
-      <h2 class="title">Produtividade</h2>
+      <h2 class="title"><i class="icon fas fa-tachometer-alt"></i>Produtividade</h2>
       <p class="description">O Alterdata Pack importa os dados e lança a nota no Escrita Fiscal. Você será notificado da importação através do Gerente Eletrônico Alterdata. É possível visualizar e fazer o download do XML ou da DANFE sempre que precisar.</p>
     </div>
 
     <div class="product">
-      <i class="fas fa-ad"></i>
-      <h2 class="title">Segurança</h2>
+      <h2 class="title"><i class="icon fas fa-shield-alt"></i></i>Segurança</h2>
       <p class="description">O NF-Stock protege seu cliente contra fraudes! Todas as notas emitidas contra o CNPJ do seu cliente serão automaticamente baixadas do site da Receita Federal ou da Prefeitura, evitando que as notas geradas indevidamente passem despercebidas.</p>
     </div>
   </section>
