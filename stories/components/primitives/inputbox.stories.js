b1508e05ba02057becd07c14d23ff7a629c71896
iFgR <ifgr.fagner@gmail.com>
2019-03-29 20:56:21 -0300

    feat: Create Inputbox object

---
+++ stories/components/primitives/inputbox.stories.js
@@ -0,0 +1,60 @@
+import { storiesOf } from "@storybook/vue";
+import FullBanner from '../../../src/components/full-banner/full-banner';
+
+storiesOf("Primitves / inputbox", module)
+  .add("default", () => ({
+    components: { FullBanner },
+    template: `
+      <section style="background-color: #F9F9F9; width: 100%; height: 100%;">
+        <fieldset style="width: 500px; height: 200px; padding: 3em;">
+          <label class="input-label">
+            <input
+              class="input" type="text"
+              value=""
+              placeholder="Experiment Text"
+              required>
+            
+            <span class="placeholder">Experiment Text</span>
+          
+            <div class="iconcontainer">
+              <i class="icon fas fa-shield-alt"></i>
+            </div>
+
+            <div class="iconcontainer -warning">
+              <i class="icon fas fa-exclamation-triangle"></i>
+            </div>
+          </label>
+        </fieldset>
+      </section>
+    `
+  }))
+  .add("warning", () => ({
+    components: { FullBanner },
+    template: `
+    <section style="background-color: #F9F9F9; width: 100%; height: 100%;">
+        <fieldset style="width: 500px; height: 200px; padding: 3em;">
+          <label class="input-label -warning">
+            <input
+              class="input" type="text"
+              value=""
+              placeholder="Experiment Text"
+              required>
+            
+            <span class="placeholder">Experiment Text</span>
+          
+            <div class="iconcontainer">
+              <i class="icon fas fa-shield-alt"></i>
+            </div>
+
+            <div class="iconcontainer -warning">
+              <i class="icon fas fa-exclamation-triangle"></i>
+            </div>
+          </label>
+        </fieldset>
+      </section>
+  `
+  }))
+
+  // < span class="label" > Nome da pessoa</span >
+  //   <i class="icon fas fa-shield-alt"></i>
+  //   <input class="input" type="text" value="Experiment Text">
